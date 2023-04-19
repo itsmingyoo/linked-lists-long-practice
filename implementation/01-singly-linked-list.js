a7f6f60a5b3d0ee1bbd3740496e39d135457bfff
Minh Tran <minhttran93@gmail.com>
2023-04-19 15:45:19 -0700

    finished singly-linked-list-with-david-olivier

--- implementation/01-singly-linked-list.js
+++ implementation/01-singly-linked-list.js
@@ -24,7 +24,7 @@ class SinglyLinkedList {
     }
 
     addToTail(val) {
-        // There are bugs in this method! Fix them!!!
+        // There are BUGS in this method! Fix them!!!
 
         // Add node of val to tail of linked list
         let newNode = new SinglyLinkedNode(val);
@@ -47,11 +47,11 @@ class SinglyLinkedList {
 
     removeFromHead() {
         // Remove node at head
-        if(!this.head) return undefined;
-        const oldHead = this.head;
-        this.head = this.head.next;
-        this.length--;
-        return oldHead;
+        if(!this.head) return undefined; // if no head return undefined
+        const oldHead = this.head; // store old head
+        this.head = this.head.next; // give old head the value of the next head
+        this.length--; // decrement
+        return oldHead; // return
 
         // Write your hypothesis on the time complexity of this method here
         // O(1)
@@ -59,17 +59,20 @@ class SinglyLinkedList {
 
     removeFromTail() {
         // Remove node at tail
-        if(!this.head) return undefined;
+        if(!this.head) return undefined; // if no head, return undefined
 
-        let curr = this.head;
-        let prev;
-        while (curr.next) {
-            prev = curr;
-            curr = curr.next;
+        let curr = this.head; // store current head to curr
+        let prev; // declare a 'previous' variable so we can constantly change it through our while loop iteration
+
+        //we are traversing to the end of the list; the end of the list is when the next head === null
+        while (curr.next) { // while there is still a next head, execute this loop
+            prev = curr; // store current head as 'prev' previous
+            curr = curr.next; // store the next head as the current head
         }
-        if(!prev) this.head = null;
-        else prev.next = null;
-        this.length--;
+
+        if(!prev) this.head = null; // if no prev value, make the current head = null
+        else prev.next = null; // else // make the prev.next to null
+        this.length--; // increment
         return curr;
 
         // Write your hypothesis on the time complexity of this method here
@@ -77,18 +80,32 @@ class SinglyLinkedList {
 
     peekAtHead() {
         // Return the value of head node
-
-        // Your code here
+        // console.log(this)
+        if (!this.head) return undefined;
+        return this.head.value;
 
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
     print() {
         // Print out the linked list
+        if (!this.head) return;
 
-        // Your code here
+        // Should console.log the values of the nodes in the linked list in order from head to tail
+        if (this.head) {// if there's a head execute the while loop inside
+            let curr = this.head; // DO NOT USE CONST;
+            //stores the current head
 
+            //traverse the linkedlist
+            while (curr) {//while there is still a current value aka if the next value isn't null that is stored into here, then continue the loop; if curr.next is null then is saved as curr; the loop will end
+                console.log(curr.value) // curr.value is the value that we're outputting before we change it to the next one
+                curr = curr.next; //change curr value to the next;
+            }
+        }
+        return;
         // Write your hypothesis on the time complexity of this method here
+        //O(n)
     }
 }
 

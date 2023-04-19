7ed479a08fc84dbcb48a7d7654da7f50189bf8eb
Minh Tran <minhttran93@gmail.com>
2023-04-19 13:57:07 -0700

    coolio

--- implementation/01-singly-linked-list.js
+++ implementation/01-singly-linked-list.js
@@ -13,33 +13,36 @@ class SinglyLinkedList {
     }
 
     addToHead(val) {
-        // Add node of val to head of linked list
-
-        // Your code here
+        const ll = new SinglyLinkedNode(val); //create new ll node
+        ll.next = this.head; //newly created node, look at old head
+        this.head = ll; //old head will be new head
+        this.length++;//increment
+        return this;
 
         // Write your hypothesis on the time complexity of this method here
+        // O(1)
     }
 
     addToTail(val) {
         // There are bugs in this method! Fix them!!!
 
         // Add node of val to tail of linked list
-        let newNode = new SinglyLinkedNode(data);
+        let newNode = new SinglyLinkedNode(val);
+        this.length++;
 
-        if (!head) {
-            head = newNode;
-            return head;
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let curr = this.head;
+            while (curr.next) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
         }
-
-        let curr = head;
-        while (curr) {
-            curr = current.next;
-        }
-        curr.next = newNode;
-
-        return head;
+        return this;
 
         // Write your hypothesis on the time complexity of this method here
+        // o(1)
     }
 
     removeFromHead() {

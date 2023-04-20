ce0483002a569a412d4ff96ab53dfa3cd70ff349
Minh Tran <minhttran93@gmail.com>
2023-04-19 23:19:36 -0700

    finished queue SOLO BABBYYYYYYY jk we factor with seabass

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -13,21 +13,21 @@ class Queue {
         const newNode = new SinglyLinkedNode(val);
 
         //Should reassign only the tail pointer when new nodes are added to the tail of the queue of size one or greater
-        if (this.length >= 1) {
-            this.head = this.head.next;
-            while (this.tail.prev) {
-                //cbt
-                newNode.prev = this.tail;
-                this.tail.next = newNode;
+        //this assignment puts very high emphasis on the part where you only change the tail pointer IF the fucking new node is added to the god damn tail
+        if (this.length >= 1) { // if length is greater than or equal to 1, execute this
 
-                this.tail = newNode;
+            //we MUST use this.head to traverse if we don't have prev aka singlylinkedlists
+            let curr = this.head; // assign curr to this.head ;
+            while (curr.next) { // traverse if curr.next exists aka this.head.next exists aka if next node is not null
+                curr = curr.next; // this is essentially i++ until curr.next does not exist or is null
             }
-
+            //bc we found that curr.next is null, we exit the loop
+            curr.next = newNode;//1. assign curr.next to the newNode
+            this.tail = newNode;//2. change tail pointer to the newNode
         } else {
             this.head = newNode;
             this.tail = newNode;
         }
-
         this.length++;
         return this.length;
 
@@ -36,8 +36,20 @@ class Queue {
 
     dequeue() {
         // Remove node from front of queue (linked list) shift method
+        if (!this.length) return null; //if length === 0 return null
 
-        // Your code here
+        const headValue = this.head.value; //store and return this original head value
+
+        if (this.length === 1) {//if length is 1 or there is only 1 item, set both head/tail to null
+            this.head = null;//remove head
+            this.tail = null;//remove tail
+        }
+        if (this.length >= 2) {//if length >= 2
+            this.head = this.head.next;//set new current value to the next node's head value
+            // this.head = null;
+        }
+        this.length--;//decrement
+        return headValue;//return the original headValue;
 
         // Write your hypothesis on the time complexity of this method here
     }
